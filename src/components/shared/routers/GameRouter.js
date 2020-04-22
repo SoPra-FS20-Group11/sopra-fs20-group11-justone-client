e9df305620544a6e6fcc15703eb08e97ace1b973
Venusan Velrajah <34336015+venuking@users.noreply.github.com>
2020-04-22 19:28:57 +0200

    Update GameRouter.js

--- src/components/shared/routers/GameRouter.js
+++ src/components/shared/routers/GameRouter.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Redirect, Route } from "react-router-dom";
 import GameLobby from "../../game/GameLobby";
 import DrawCard from "../../game/DrawCard";
+import Clues from "../../game/Clues";
 
 const Container = styled.div`
   display: flex;
@@ -18,14 +19,27 @@ class GameRouter extends React.Component {
       <Container>
         <Route
           exact
+<<<<<<< HEAD
           path={`${this.props.base}/lobby`}
           render={() => <GameLobby />}
+=======
+          path={`${this.props.base}/drawphase`}
+          render={() => <DrawCard />}
         />
-
+        <Route
+          exact
+          path={`${this.props.base}/clues`}
+          render={() => <Clues />}
+>>>>>>> 648fd73a4b29a8758247f16d86e0985bd7a047c0
+        />
         <Route
           exact
           path={`${this.props.base}`}
+<<<<<<< HEAD
           render={() => <Redirect to={`${this.props.base}/lobby`} />}
+=======
+          render={() => <Redirect to={`${this.props.base}/drawphase`} />}
+>>>>>>> 648fd73a4b29a8758247f16d86e0985bd7a047c0
         />
       </Container>
     );
